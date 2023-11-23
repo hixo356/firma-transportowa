153d4586fb92a6e5a189623d62fa2d7ef0212e7a
hixo356 <120134199+hixo356@users.noreply.github.com>
2023-11-23 23:23:42 +0100

    test

--- myproject/settings.py
+++ myproject/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'firma-transportowa',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
