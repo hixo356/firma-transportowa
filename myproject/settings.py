d54c1cddba6a29c2104d9cb26dfc444f1283b5ca
wasilewskiJ <wasilewski.jakub@outlook.com>
2024-01-13 23:51:50 +0100

    frontend
    front do wszystkiego

--- myproject/settings.py
+++ myproject/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'firma-transportowa',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'Dzikakuna321',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
